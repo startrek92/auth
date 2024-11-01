081bc0c4188711584dcf1ea85485eae842d590a5
Amit <amits12m@gmail.com>
2024-11-01 22:50:31 +0530

    used record

--- src/main/java/com/promptdb/auth/controller/error/DefaultController.java
+++ src/main/java/com/promptdb/auth/controller/error/DefaultController.java
@@ -2,18 +2,12 @@ package com.promptdb.auth.controller.error;
 
 import org.springframework.http.*;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DefaultController {
 
-     static class DefaultResponse {
-        public final String message;
-
-        public DefaultResponse(String message) {
-            this.message = message;
-        }
+    record DefaultResponse(String message) {
     }
 
     @RequestMapping(path = "/**")
