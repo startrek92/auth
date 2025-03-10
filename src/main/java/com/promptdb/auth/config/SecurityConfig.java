ddd226c783cc2cfb56740c600a68d32ccc65087e
Amit <amits12m@gmail.com>
2025-03-10 22:52:07 +0530

    import updates

--- src/main/java/com/promptdb/auth/config/SecurityConfig.java
+++ src/main/java/com/promptdb/auth/config/SecurityConfig.java
@@ -1,6 +1,5 @@
 package com.promptdb.auth.config;
 
-import com.mysql.cj.protocol.AuthenticationProvider;
 import com.promptdb.auth.filter.JwtFilter;
 import com.promptdb.auth.services.AuthUserDetailsService;
 import jakarta.servlet.http.HttpServletResponse;
@@ -12,11 +11,8 @@ import org.springframework.security.authentication.dao.DaoAuthenticationProvider
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
