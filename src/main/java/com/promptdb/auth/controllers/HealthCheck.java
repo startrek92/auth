9f6b9b6b6a4564a75fa798db33f2f70199757cf4
Amit <amits12m@gmail.com>
2025-11-19 23:09:12 +0530

    added logger config

--- src/main/java/com/promptdb/auth/controllers/HealthCheck.java
+++ src/main/java/com/promptdb/auth/controllers/HealthCheck.java
@@ -1,6 +1,7 @@
 package com.promptdb.auth.controllers;
 
 import com.promptdb.auth.dto.HealthCheckResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,10 +10,16 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/health")
+@Slf4j
 public class HealthCheck {
 
     @GetMapping("")
     public ResponseEntity<HealthCheckResponse> healthCheck() {
+        log.info("info health check endpoint");
+        log.debug("debug health check endpoint");
+        log.error("error health check endpoint");
+        log.warn("warn health check endpoint");
+        log.trace("trace health check endpoint");
         HealthCheckResponse healthCheckResponse = new HealthCheckResponse("ok", "qwerty");
         return new ResponseEntity<HealthCheckResponse>(healthCheckResponse, HttpStatus.OK);
     }
